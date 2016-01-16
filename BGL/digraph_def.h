6056c504ed028c8d6ed240263266b78d0b27bc24
xing <wistar.sjtu@gmail.com>
2016-01-16 18:51:45 +0800

    git ignore fix

--- BGL/digraph_def.h
+++ BGL/digraph_def.h
@@ -32,7 +32,7 @@ Graph digraph_toy_ex(){// return a toy example of directed weighted graph (V=5, 
     return G;
 }
 
-Graph DAG_toy_ex(){ // return a DAG toy example
+Graph DAG_toy_ex(){ // return a DAG toy example (without edge weights)
     int us[] = {0,2,2,0,1,4};
     int vs[] = {1,4,5,3,4,3};
     Graph G(6);
@@ -41,3 +41,17 @@ Graph DAG_toy_ex(){ // return a DAG toy example
     return G;
 }
 
+Graph maxflow_toy_ex(){// return a toy example of directed weighted graph (V=6, E=8, s=4, t=5)
+    Graph G(6); 
+    WeightMap wm = get(edge_weight, G); // associate wm with G --> G is edge-weighted
+    int us[] = {0,1,4,0,2,2,3,4};
+    int vs[] = {1,5,0,3,1,3,5,2};
+    int cs[] = {1,2,2,1,1,1,1,1};
+    rep(i,8){
+        Edge e;
+        tie(e, tuples::ignore) = add_edge(us[i], vs[i], G); 
+        wm[e] = cs[i];
+    }
+    return G;
+}
+,
